50bb57bc82ee2c092244e5b32650053b321ab228
Marina-Mironova <kenderrisha1@gmail.com>
2023-06-05 11:19:26 +0200

    Bot was created. It cans answer to the command "/start" and when command is not recognized.

---
+++ src/main/java/com/project/telegrambot/config/BotInitializer.java
@@ -0,0 +1,29 @@
+package com.project.telegrambot.config;
+
+import com.project.telegrambot.service.NewsFromSiteBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+public class BotInitializer {
+
+    @Autowired
+    NewsFromSiteBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException{
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        }
+        catch (TelegramApiException e) {
+
+        }
+    }
+}
