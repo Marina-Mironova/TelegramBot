4572c45f819ed0d8c796c75d77ee75cfb4782d07
Marina-Mironova <kenderrisha1@gmail.com>
2024-01-03 20:02:23 +0100

    Slf4j annotation was added + logback.xml ie was created

--- src/main/java/com/project/telegrambot/config/BotInitializer.java
+++ src/main/java/com/project/telegrambot/config/BotInitializer.java
@@ -1,6 +1,7 @@
 package com.project.telegrambot.config;
 
 import com.project.telegrambot.service.NewsFromSiteBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -10,6 +11,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramBot;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 @Component
 public class BotInitializer {
 
@@ -23,7 +25,7 @@ public class BotInitializer {
             telegramBotsApi.registerBot(bot);
         }
         catch (TelegramApiException e) {
-
+            log.error("Error occured: " + e.getMessage());
         }
     }
 }
--- src/main/java/com/project/telegrambot/service/NewsFromSiteBot.java
+++ src/main/java/com/project/telegrambot/service/NewsFromSiteBot.java
@@ -3,6 +3,7 @@ package com.project.telegrambot.service;
 import com.project.telegrambot.config.BotConfig;
 import com.project.telegrambot.model.User;
 import com.project.telegrambot.model.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -17,10 +18,12 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static com.project.telegrambot.model.User.*;
 
 
+@Slf4j
 @Component
 public class NewsFromSiteBot extends TelegramLongPollingBot {
 
@@ -38,13 +41,8 @@ public class NewsFromSiteBot extends TelegramLongPollingBot {
         List<BotCommand> listOfCommands = new ArrayList<>();
         listOfCommands.add(new BotCommand("/start", "register and get a welcome message"));
         listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
-        listOfCommands.add(new BotCommand("/active-subscriptions", "list of your active subscriptions"));
-        //listOfCommands.add(new BotCommand("/deleteData", "delete my data"));
         listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
-        listOfCommands.add(new BotCommand("/add subscription", "add subscription for your rss feed"));
-        //listOfCommands.add(new BotCommand("/unsubscribeChannel", "choose sources you don't want anymore in your rss feed"));
-        //listOfCommands.add(new BotCommand("/refreshFrequency", "choose new time interval between refreshing feed"));
-        //listOfCommands.add(new BotCommand("/stop", "stop sending new rss to you"));
+         //listOfCommands.add(new BotCommand("/stop", "stop sending new rss to you"));
         try{
             this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
 
@@ -90,7 +88,7 @@ public class NewsFromSiteBot extends TelegramLongPollingBot {
 
                 case "/mydata":
 
-                        myDataCommand(chatId);
+                        myDataCommand(chatId, update.getMessage().getChat().getUserName()); //TODO solve data type problem
                         break;
 
 
@@ -131,21 +129,24 @@ public class NewsFromSiteBot extends TelegramLongPollingBot {
     private void startCommandReceived(long chatId, String name){
 
         String answer = "Hi, " + name + ", nice to meet you!";
-
+        log.info("Replied to user " + name);
         sendMessage(chatId, answer);
     }
 
-    private void myDataCommand(long chatId) {
+    private void myDataCommand(long chatId, Message msg) {
 
-        User user = new User();
-        user.getChatId();
-        user.getUserName();
-        user.getRegisteredAt();
+      //  User chatId = User.getChatId();
+        if(userRepository.findById(msg.getChatId()).isEmpty()){
+            String  answer = "User is undefined. For registration choose 'start' command, please.";
+            sendMessage(chatId, answer);
+        }
+        else {
+            Optional<User> findChatId = userRepository.findById(chatId);
+            sendMessage(chatId, String.valueOf(msg.getChatId()) );  //TODO user data schould be here
 
-        String myData = user.toString();
-        sendMessage(chatId, myData);
+        }
     }
-
+//TODO create method user data from msg.methods and chat.get... methods
     private void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
