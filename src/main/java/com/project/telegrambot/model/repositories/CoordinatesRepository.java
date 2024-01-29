e856d2afffe707be062afd9b728210eb0c594c31
risha <kenderrisha1@gmail.com>
2024-01-29 14:56:09 +0100

    CoordinatesRepository and WeatherRepository extends CrudRepository

--- src/main/java/com/project/telegrambot/model/repositories/CoordinatesRepository.java
+++ src/main/java/com/project/telegrambot/model/repositories/CoordinatesRepository.java
@@ -1,4 +1,8 @@
 package com.project.telegrambot.model.repositories;
 
-public interface CoordinatesRepository {
+import com.project.telegrambot.model.entities.Coordinates;
+import com.project.telegrambot.model.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CoordinatesRepository extends CrudRepository<Coordinates, Long> {
 }
--- src/main/java/com/project/telegrambot/model/repositories/WeatherRepository.java
+++ src/main/java/com/project/telegrambot/model/repositories/WeatherRepository.java
@@ -1,4 +1,8 @@
 package com.project.telegrambot.model.repositories;
 
-public interface WeatherRepository {
+import com.project.telegrambot.model.entities.User;
+import com.project.telegrambot.model.entities.Weather;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WeatherRepository extends CrudRepository<Weather, Long> {
 }
