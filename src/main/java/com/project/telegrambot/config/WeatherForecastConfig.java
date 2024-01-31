8ed595ff6949562065e7ea1a0fde7933c46d295a
risha <kenderrisha1@gmail.com>
2024-01-31 15:27:47 +0100

    Configuration annotation was added to new classes

--- src/main/java/com/project/telegrambot/config/WeatherForecastConfig.java
+++ src/main/java/com/project/telegrambot/config/WeatherForecastConfig.java
@@ -1,4 +1,11 @@
 package com.project.telegrambot.config;
 
+import lombok.Data;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
 public class WeatherForecastConfig {
 }
--- src/main/java/com/project/telegrambot/service/ScheduledMessageService.java
+++ src/main/java/com/project/telegrambot/service/ScheduledMessageService.java
@@ -8,8 +8,10 @@ import com.project.telegrambot.model.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class ScheduledMessageService extends TelegramBotService{
 
     @Autowired
